e847f6208f66c67b01878887c7ac1f1f883025b7
Brayan Mamian <brayan.mamian@makrosoft.co>
2021-12-28 11:08:19 -0500

    Ajuste en secuencia de instalacion

--- instalacion/completeInstallation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -R archivos_completar/vendor ../;fuente_codigo
-cp archivos/completar/.env ../fuente_codigo
-chmod -R 777 ../prueba_tecnica;
-docker exec -it debian php /Aplicaciones/prueba_tecnica/artisan migrate --force
-printf "\n\nAplicacion inicializada correctamente\n";
